c3772a92d9f6aadaf39146ce3d24146cbe92d571
Konstantinos Railis <krailis@hotmail.com>
2019-12-17 23:13:57 +0200

    Day 1: The Tyranny of the Rocket Equation

---
+++ day_01/problem_01/d01_p01.py
@@ -0,0 +1,7 @@
+import math
+
+with open('d01_p01.txt') as f:
+    content = f.read()
+module_masses = map(int, list(content.strip().split()))
+module_fuel_req = [math.floor(x / 3) - 2 for x in module_masses]
+print(sum(module_fuel_req))
---
+++ day_01/problem_02/d01_p02.py
@@ -0,0 +1,14 @@
+import math
+
+with open('d01_p02.txt') as f:
+    content = f.read()
+module_masses = map(int, list(content.strip().split()))
+
+total_fuel_req = 0
+while True:
+    module_fuel_req = [math.floor(x / 3) - 2 for x in module_masses]
+    module_masses = [x for x in module_fuel_req if x > 0]
+    if len(module_masses) == 0:
+        break
+    total_fuel_req += sum(module_masses)
+print(total_fuel_req)
